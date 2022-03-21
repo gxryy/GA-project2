622eb17ff662a8d5e39368f4190436d9695081c3
gxrys <garysohjy@gmail.com>
2022-03-21 11:05:24 +0800

    completed data filter and processing for MW

--- src/components/Merriam.js
+++ src/components/Merriam.js
@@ -7,15 +7,16 @@ import overlook_MW from "../mock/overlook_MW";
 import minute_MW from "../mock/minute_MW";
 
 const Merriam = (props) => {
-  const [data, setData] = useState("");
+  const [APIdata, setAPIData] = useState("");
+  const processedArray = [];
   const API_KEY = "b4c04852-37ba-4237-a137-d1a1bbddd206";
   const url = `https://dictionaryapi.com/api/v3/references/collegiate/json/${props.word}?key=${API_KEY}`;
   // let display = [];
 
   // useEffect to call API upon loading
   useEffect(() => {
-    // FetchAPI(url, setData);
-    setData(channel_MW);
+    // FetchAPI(url, setAPIData);
+    setAPIData(channel_MW);
 
     return () => {
       console.log(`cleanup code: component`);
@@ -24,35 +25,43 @@ const Merriam = (props) => {
 
   // useEffect to process data it is returned
   useEffect(() => {
-    if (data) processData();
-  }, [data]);
+    if (APIdata) processData();
+  }, [APIdata]);
 
   const processData = () => {
     const mockWord = "channel";
 
-    const one = data[0];
-
-    const filteredData = data.filter(
+    const filteredData = APIdata.filter(
       (element) => element.meta.id.split(":")[0] === mockWord
     );
-    console.log(filteredData);
+    // console.log(filteredData);
 
-    const word = one.meta.id.split(":")[0];
-    const entry = one.meta.id.split(":")[1];
-    const wordType = one.fl;
-    const shortDef = one.shortdef;
-    const pronounciation = one.hwi.prs[0].sound;
+    const soundObj = filteredData[0].hwi.prs[0].sound;
+    const audio = soundObj.audio;
 
-    console.log(`word is ${word}`);
-    console.log(`entry is ${entry}`);
-    console.log(`wordType is ${wordType}`);
-    console.log(`shortDef is ${shortDef}`);
-    console.log(`pronounciation is ${pronounciation}`);
+    let subdirectory = "";
+    if (audio.startsWith("bix")) subdirectory = "bix";
+    else if (audio.startsWith("gg")) subdirectory = "gg";
+    else if (audio.match(/^(\W|_|[0-9])/g)) subdirectory = "number";
+    else subdirectory = audio.split("")[0];
+
+    const soundURL = `https://media.merriam-webster.com/audio/prons/en/us/mp3/${subdirectory}/${audio}.mp3`;
+
+    for (let i = 0; i < filteredData.length; i++) {
+      processedArray.push({
+        word: filteredData[i].meta.id.split(":")[0],
+        entry: filteredData[i].meta.id.split(":")[1],
+        wordType: filteredData[i].fl,
+        shortDef: filteredData[i].shortdef,
+        soundURL: soundURL,
+      });
+    }
+    console.log(processedArray);
 
     // card should take in word, word type , shortDef, fullDef, pronounciation
   };
 
-  // Merriam to return the defination card
+  // Merriam to return the definition card
   return (
     <div>
       <h1>This is the return from merriam</h1>
